04acf017571c99bd2331795860a9e63cf48d1e78
Miguel de Icaza <miguel@gnome.org>
2023-03-21 14:47:27 -0400

    Initial Commit

---
+++ PlaticaBot/PlaticaBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaticaBotApp.swift
+//  PlaticaBot
+//
+//  Created by Miguel de Icaza on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaticaBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
